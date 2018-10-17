edd673939acc5d6e2f3bb8310add60ae45b61c6a
suresha1991 <sureshpsg49@gmail.com>
2018-10-17 15:39:51 +0530

    First commit

---
+++ metadata.php
@@ -0,0 +1,42 @@
+<?php
+
+$sMetadataVersion = '2.0';
+
+$aModule = array(
+    'id'          => 'demopay',
+    'title'       => array(
+        'de' => 'Demopay',
+        'en' => 'Demopay',
+    ),
+    'description' => array(
+         'de' => 'demopay Zahlungsmodul',
+         'en' => 'demopay Payment module'
+    ),
+    'thumbnail'   => 'icon.png',
+    'version'     => '1.2.1',
+    'author'      => 'demopay',
+    'url'         => 'https://www.abcxyz.com',
+    'email'       => 'technic@abcxyz.com',
+    'extend'      => array(
+     \OxidEsales\Eshop\Application\Controller\PaymentController::class        => \oe\demopay\Controller\PaymentController::class,
+     \OxidEsales\Eshop\Application\Model\PaymentGateway::class                => \oe\demopay\Model\PaymentGateway::class,
+     \OxidEsales\Eshop\Application\Model\Order::class                         => \oe\demopay\Model\Order::class,
+     
+    ),
+    'controllers'       => array(),
+    'files'       => array(),
+    'templates'   => array(),
+    'blocks'      => array(
+                            array(
+                                'template' => 'page/checkout/payment.tpl',
+                                'block'    => 'select_payment',
+                                'file'     => '/views/blocks/page/checkout/demopaypayments.tpl'
+                                )
+                        ),
+    'settings'     => array(),
+    'events'       => array(
+        'onActivate'   => '\oe\demopay\Core\Events::onActivate',
+        'onDeactivate' => '\oe\demopay\Core\Events::onDeactivate'
+    ),
+);
+
